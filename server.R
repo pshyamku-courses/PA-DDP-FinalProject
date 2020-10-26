e698ccd47df049705cef96166e70fc6b6851654d
pshyamku <prashanth@nanowearinc.com>
2020-10-25 22:05:28 -0400

    Initial commit

--- server.R
+++ server.R
@@ -9,18 +9,55 @@
 
 library(shiny)
 
+# Read the data that is needed
+library(ggplot2)
+library(forecast)
+library(tsbox)
+library(plotly)
+
+data <- read.csv("Data/NCHS_-_Age-adjusted_Death_Rates_for_Selected_Major_Causes_of_Death.csv")
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    
+    v <- reactiveValues(doPlot = FALSE)
+    
+    observeEvent(input$go, {
+        # 0 will be coerced to FALSE
+        # 1+ will be coerced to TRUE
+        v$doPlot <- input$go
     })
+    
+    fit <- reactive({
+        
+        # generate bins based on input$bins from ui.R
+        selected_disease <- input$disease_name
+        y    <- subset(data, 
+                       Cause == input$disease_name, select = c(Year, Age.Adjusted.Death.Rate))
 
+        fit <- ets(ts_ts(ts_long(y)), allow.multiplicative.trend = TRUE)
+        
+    })
+    
+    output$forecastPlot <- renderPlot({
+        if (v$doPlot == FALSE) return()
+        
+        isolate({
+           
+            # draw the histogram with the specified number of bins
+            autoplot(forecast(fit(), h = input$forecast_window)) + geom_forecast() +
+                ggtitle("Forecast of Age Adjusted Death Rate") + xlab("Years") +
+                ylab("Age Adjusted Death Rate") +
+                theme(text = element_text(size=20))
+        })
+    })
+    
+    output$pastDataPlot <- renderPlotly({
+
+        data %>% plot_ly(x = ~Year, y = ~Age.Adjusted.Death.Rate,
+                color = ~Cause, type = "scatter",
+                mode = "lines") %>%
+        layout(title = "All causes", xaxis = list(title = "Years", titlefont = 20),
+               yaxis = list(title = "Age Adjusted Death Rate", titlefont = 20))
+    })
 })
--- ui.R
+++ ui.R
@@ -8,26 +8,42 @@
 #
 
 library(shiny)
+library(plotly)
+library(ggplot2)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
+    titlePanel("US major causes of death"),
+    
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
+            p("Select the disease for which you would like to forecast 
+              death rates."),
+            selectInput("disease_name", "Major Disease:",
+                        c("Heart Disease" = "Heart Disease",
+                          "Cancer" = "Cancer",
+                          "Accidents" = "Accidents",
+                          "Stroke" = "Stroke",
+                          "Influenza and Pneumonia" = 
+                              "Influenza and Pneumonia")),
+            p("Select the number of years into the future that you would
+              like to project death rates"),
+            sliderInput("forecast_window",
+                        "Years to Forecast:",
                         min = 1,
-                        max = 50,
-                        value = 30)
+                        max = 30,
+                        value = 30),
+            p("Click on the Update forecast button to generate the forecast 
+              plot for the chosen disease and the forecast period"),
+            actionButton("go", "Update forecast")
         ),
-
+        
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+            plotlyOutput("pastDataPlot"), plotOutput("forecastPlot")
         )
     )
 ))
